19712a748f9185bb760142e50df5e5d71895d27f
dherrada <dylan.herrada@gmail.com>
2020-03-15 16:58:20 -0400

    Ran black, updated to pylint 2.x

--- examples/ds3231_simpletest.py
+++ examples/ds3231_simpletest.py
@@ -5,16 +5,18 @@
 
 import time
 import board
+
 # For hardware I2C (M0 boards) use this line:
 import busio as io
+
 # Or for software I2C (ESP8266) use this line instead:
-#import bitbangio as io
+# import bitbangio as io
 
 import adafruit_ds3231
 
 
 i2c = io.I2C(board.SCL, board.SDA)  # Change to the appropriate I2C clock & data
-                                    # pins here!
+# pins here!
 
 # Create the RTC instance:
 rtc = adafruit_ds3231.DS3231(i2c)
@@ -23,23 +25,27 @@ rtc = adafruit_ds3231.DS3231(i2c)
 days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 
 
-#pylint: disable-msg=bad-whitespace
-#pylint: disable-msg=using-constant-test
-if False:   # change to True if you want to set the time!
+# pylint: disable-msg=bad-whitespace
+# pylint: disable-msg=using-constant-test
+if False:  # change to True if you want to set the time!
     #                     year, mon, date, hour, min, sec, wday, yday, isdst
-    t = time.struct_time((2017,  10,   29,   15,  14,  15,    0,   -1,    -1))
+    t = time.struct_time((2017, 10, 29, 15, 14, 15, 0, -1, -1))
     # you must set year, mon, date, hour, min, sec and weekday
     # yearday is not supported, isdst can be set but we don't do anything with it at this time
-    print("Setting time to:", t)     # uncomment for debugging
+    print("Setting time to:", t)  # uncomment for debugging
     rtc.datetime = t
     print()
-#pylint: enable-msg=using-constant-test
-#pylint: enable-msg=bad-whitespace
+# pylint: enable-msg=using-constant-test
+# pylint: enable-msg=bad-whitespace
 
 # Main loop:
 while True:
     t = rtc.datetime
-    #print(t)     # uncomment for debugging
-    print("The date is {} {}/{}/{}".format(days[int(t.tm_wday)], t.tm_mday, t.tm_mon, t.tm_year))
+    # print(t)     # uncomment for debugging
+    print(
+        "The date is {} {}/{}/{}".format(
+            days[int(t.tm_wday)], t.tm_mday, t.tm_mon, t.tm_year
+        )
+    )
     print("The time is {}:{:02}:{:02}".format(t.tm_hour, t.tm_min, t.tm_sec))
-    time.sleep(1) # wait a second
+    time.sleep(1)  # wait a second
