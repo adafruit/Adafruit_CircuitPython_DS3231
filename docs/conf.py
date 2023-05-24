fc3a6a651ee6211b02903ec0bc6808b359bf6d4b
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2023-05-23 22:32:24 -0400

    Update .pylintrc, fix jQuery for docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinxcontrib.jquery",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
